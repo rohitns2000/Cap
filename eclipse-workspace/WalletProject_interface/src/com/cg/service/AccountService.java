dbc1b4af6847c5baa80f51d2b6b5ae970a145337
rohitns2000 <51941325+rohitns2000@users.noreply.github.com>
2019-06-18 10:14:24 +0530

    Add files via upload

---
+++ eclipse-workspace/WalletProject_interface/src/com/cg/service/AccountService.java
@@ -0,0 +1,46 @@
+package com.cg.service;
+
+import com.cg.bean.Account;
+
+public class AccountService implements Gst,Transaction{
+
+	@Override
+	public double withdraw(Account ob, double amount) {
+			// TODO Auto-generated method stub
+			double new_balance=ob.getBalance()-amount;
+			if(new_balance < 1000)
+			{
+				new_balance=ob.getBalance();
+				System.out.println("Insufficient Balance");
+			}
+			ob.setBalance(new_balance);
+			return new_balance;
+		
+	}
+
+	@Override
+	public double deposit(Account ob, double amount) {
+		// TODO Auto-generated method stub
+		return ob.getBalance()+amount;
+	}
+
+	@Override
+	public double transferMoney(Account from, Account to, double amount) {
+		if(from.getBalance() < 1000.00)
+		{
+		from.setBalance(from.getBalance()-amount);
+		to.setBalance(to.getBalance()+amount);
+		return to.getBalance();
+		}
+		else
+			System.out.println("Insufficient balance");
+		return 0.00;
+	}
+
+	@Override
+	public double calculateTax(double PCT, double amount) {
+		// TODO Auto-generated method stub
+		return amount*Gst.PCT_5;
+	}
+
+}
